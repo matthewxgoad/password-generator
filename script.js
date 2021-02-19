398ba52f5727d30b9e199cd93811df41892b4cde
Matthew Goad <matthewxgoad@gmail.com>
2021-02-19 08:54:07 -0500

    added initial user prompts for password requirements

--- script.js
+++ script.js
@@ -1,14 +1,65 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Create a series of prompts asking the user the parameters of their new password.
+// This function asks and sets the password length.
+function howLong(){
+    passwordLength = prompt("How long would you like your password to be? (between 8-128 characters)");
+        if( isNaN(passwordLength)){
+            alert("Come on, dude. That's not a number.");
+         howLong;
+        }else if( passwordLength < 8 ){
+            alert("That's too short, dummy. Password must be between 8-128 characters.");
+         howLong();
+        }else if( passwordLength > 128 ){
+            alert("That's too dang long. Password must be between 8-128 characters.");
+         howLong;
+        }
+        return passwordLength;
+    }   
+// This function prompts user to use or not use UPPERCASE characters.
+function useUppercase(){
+    uppercaseConfirm = confirm("Use UPPERCASE characters? OK to confirm or Cancel for No.");
+    return uppercaseConfirm;
+}
+// This function prompts user to use or not use lowercase characters.
+function useLowercase(){
+    lowercaseConfirm = confirm("Use lowercase characters? OK to confirm or Cancel for No.");
+    return lowercaseConfirm;
+}
+// This function prompts user to use or not use numbers.
+function useNumbers(){
+    numbersConfirm = confirm("Use lowercase characters? OK to confirm or Cancel for No.");
+    return numbersConfirm;
+}
+// This function prompts user to use or not use special characters.
+function useSpecialCharacters(){
+    specialConfirm = confirm("Use lowercase characters? OK to confirm or Cancel for No.");
+    return specialConfirm;
+}
+// Create a function to combine the input of previous prompts and generate random characters into the var password
+function generatePassword() {
+    howLong();
+    useUppercase();
+    useLowercase();
+    useNumbers();
+    useSpecialCharacters();
+    var characters = ""
+    var passwordLength = ""
+    
+}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  // text area has a property called value, set it to the new password
+    passwordText.value = password;
+  
+  }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Go the extra mile and add a Copy to Clipboard function?
+
---
+++ script0.js
@@ -0,0 +1,112 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+const lowercase = [...'abcdefghijklmnopqrstuvwxyz'];
+const uppercase = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+const numerals = [...'0123456789'];
+const specialCharacters = [...'!@#$%^&*()~-=_+,.{}'];
+
+
+function generatePassword() {
+  // const lowercase = [...'abcdefghijklmnopqrstuvwxyz'];
+  // const uppercase = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+  // const numerals = [...'0123456789'];
+  // const specialCharacters = [...'!@#$%^&*()~-=_+,.{}'];
+  prompt("How many characters should your password contain?");
+}
+/* 
+Required:
+uppercase, lowercase, numeric, and/or special characters
+At least 8 characters; no more than 128 characters
+Series of prompts:
+true or false, do you want to include lowercase? if so add lowercase characters
+
+a variable like allCharactersPossible to store all possible characters
+and then pull those characters randomly
+
+
+var dog = ["d", "o", "g"]
+var cat = ["c", "a", "t"]
+
+var allAnimalCharacters = ???
+
+random selection for n number of characters the user wants
+validation to meet assignement criteria, also NaN (not a number)
+check NaN first
+
+
+1 How many characters would you like your password to contain?
+Password must between 8-128 chracters
+Click OK to confirm including special chracters
+Click ok to include … (cancel would exclude)
+prints password meeting criteria
+
+"To include special characters, click OK. To exclude special characters, click Cancel."
+
+The HTML portion is just changing the text-content at the end. Also making the button work. 
+
+*/
+
+
+
+// This function gets a random integer between and including two values.
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); 
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+// look up event listener
+// text area has a property called value, set it to the new password
+generateBtn.addEventListener("click", writePassword);
+
+
+/**
+ * Required: 
+ * lowercase, uppercase, numeric, and/or special characters
+ * 
+ * 8 <= passwordLength <= 128
+//GIVEN a new password -- our application's purpose
+//prompt, window.prompt
+//we're asking the user what they want for their password
+// also another series of prompts
+//what length? we store that
+//what characters ? we store that too
+  var allCharactersPossible;
+  var dog = ["d", "o", "g"]
+  var cat = ["c", "a", "t"]
+  var allAnimalCharacters = ? // how do we do this given variables cat and dog?
+  2) random selection for n number of characters the user wanted
+          1) NaN 
+          2) Validation btw 8 -128
+  if(lowerCase)
+  {
+    add lowercase to potential password characters
+  }
+  if(uppercase)
+  {
+    add uppercase to potential password characters
+  }
+*/
+
+// function makeid(length) {
+//   var result           = '';
+//   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//   var charactersLength = characters.length;
+//   for ( var i = 0; i < length; i++ ) {
+//      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//   }
+//   return result;
+// }
+
+// console.log(makeid(5));
